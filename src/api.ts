01ec07898c9bed266610b437e8e0a87a8db3ea47
wajahatsarwat <wajahat.sarwat@m3tech.com.pk>
2025-07-09 15:26:43 -0700

    api file updated

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const BASE_URL = 'http://localhost:5000/'
-const BASE_URL = 'http://13.204.43.245/whatsapp-backend/'
+const BASE_URL = 'http://13.203.249.84/whatsapp-backend/'
 
 const API = axios.create({
     baseURL: BASE_URL,
